680b63fed041195aa54de122604c662c5f58e00e
Ashley McDonnell <ash.m.mcdonnell@gmail.com>
2018-01-18 10:48:29 +1000

    refactored JS to be more modular

--- excuses.js
+++ excuses.js
@@ -1,22 +1,31 @@
 function addExcuse() {
 
-	var myPerson = ['dog', 'best friend', 'partner', 'crush', 'granny', 'pop'];
-	var action = [' ate', ' caught', ' livetweeted',  ' gave my dog', ' vomitted on', ' lost', ' misplaced', ' ebayed', ' craigslisted', ' snapchatted'];
-    var myItem = ['my Sex Tape', "Zika Virus" , "Alzheimer's Disease", 'Netflix and Chill', "Avian Influenza"];
-
-	var who = myPerson[Math.round(Math.random()*(myPerson.length-1))];
-	var did = action[Math.round(Math.random()*(action.length-1))];
-	var what = myItem[Math.round(Math.random()*(myItem.length-1))];
+    //declare the arrays
+	const myPerson = ['dog', 'best friend', 'partner', 'crush', 'granny', 'pop'];
+	const action = [' ate', ' caught', ' livetweeted',  ' gave my dog', ' vomitted on', ' lost', ' misplaced', ' ebayed', ' craigslisted', ' snapchatted'];
+    const myItem = ['Sex Tape', "Zika Virus" , "Alzheimer's Disease", 'Netflix and Chill', "Avian Influenza"];
     
+    //select a random item from the array and store it in a variable
+	/*let who = myPerson[Math.round(Math.random()*(myPerson.length-1))];
+	let did = action[Math.round(Math.random()*(action.length-1))];
+	let what = myItem[Math.round(Math.random()*(myItem.length-1))]; */
     
-
+    //turn the randomising of the array into a reusable function
+    let randNum = function randNumWord(array) {
+        var arrayIndex = Math.round(Math.random()*(array.length - 1));
+        return array[arrayIndex];
+    }
+    
+    //use Jquery to print the random variables in a sentance to the homepage
 	$(document).ready (function () {
         $('#generate').click (function () {
         $("#excusesComplete").text(function(n) {
-            return "My " + who + did + " my " + what + ".";
+            return "My " + randNum(myPerson) + randNum(action) + " my " + randNum(myItem) + ".";
         });
         });
     });
 
 }
+
+
 
